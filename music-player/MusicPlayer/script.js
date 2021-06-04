848e5dbd140cd29e1006a6758f89e722f3424810
cuong0906 <cuong090694@gmail.com>
2021-06-04 17:27:43 +0800

    Add files via upload

---
+++ music-player/MusicPlayer/script.js
@@ -0,0 +1,95 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+//Song titles
+const songs = ['Chỉ là không cùng nhau']
+
+//Keep track of songs
+let songIndex = 0
+
+//Initially load song info DOM
+loadSong(songs[songIndex])
+
+//Update song details
+function loadSong(song){
+    title.innerHTML = song
+    audio.src= `assets/music/${song}.mp3`
+    cover.src= `assets/img/${song}.jpg` 
+}
+
+function playSong(){
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+function pauseSong(){
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong(){
+    songIndex--
+
+    if(songIndex < 0){
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong(){
+    songIndex++
+
+    if(songIndex > songs.length - 1){
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPresent = (currentTime / duration) * 100
+    progress.style.width = `${progressPresent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+// Events listeners
+playBtn.addEventListener('click', ()=>{
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying){
+        pauseSong()
+    }else{
+        playSong()
+    }
+})
+
+//Change songs events
+prevBtn.addEventListener('click',prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+progressContainer.addEventListener('click', setProgress)
